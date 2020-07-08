f71c0e97fa6186718d34156102c45cf0edd3fe6e
Tobias Wittwer <t.wittwer95@gmail.com>
2020-07-08 21:24:06 +0200

    build: add commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        // Minor
+        'feat',
+        // Patch
+        'fix',
+        'build',
+        'refactor',
+        'perf',
+        // None
+        'docs',
+        'chore',
+        'ci',
+        'test',
+        'revert',
+      ],
+    ],
+    'scope-empty': [0],
+    'scope-enum': [
+      2,
+      'always',
+      // prettier-ignore
+      [
+      ],
+    ],
+  },
+  ignores: [(message) => message.toUpperCase().startsWith('WIP')],
+};
